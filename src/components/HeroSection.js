32453542904b07d9a0039919871922f1b739b9d8
ParasGada <parasgada0@gmail.com>
2021-07-25 22:49:35 +0530

    final product

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from './Button';
+import "./HeroSection.css";
+import "../App.css";
+
+function HeroSection(){
+        return (
+            <div className="hero-container">
+                <h1>ADVENTURE AWAITS</h1>
+                <p>What are you waiting for?</p>
+                <div className="hero-btns">
+                    <Button buttonStyle="btn--outline" buttonSize="btn--large" className="btns">
+                        GET STARTED
+                    </Button>
+                    <Button buttonStyle="btn--primary" buttonSize="btn--large" className="btns">
+                        WATCH TRAILER<i className="far fa-play-circle"/>
+                    </Button>
+                </div>
+            </div>
+        )    
+}
+
+export default HeroSection;
